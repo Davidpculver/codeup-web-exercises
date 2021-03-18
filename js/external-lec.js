1a9bb0f648bda3cde46c09b8fa9eeb8ea7835dde
David Culver <culver.david.p@gmail.com>
2021-03-18 15:28:27 -0500

    completed the js with html lecture

---
+++ js/external-lec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//var message = "Howdy y'all!";
+
+//console.log(message);
+
+//**do not key in the word message inside the prompt
+// var num = parseFloat(prompt("give me a number"));
+// console.log("user entered: " + num + ". That number plus 17 is: " + (num + 17) + ".")
+
+//confirm returns true or false upon selecting the pop up
+var canIGoToToysRUs = confirm("Can i go to toys r us, please?");
+console.log(canIGoToToysRUs);
+
+var valueOfThis = alert("You're looking at a website!");
+console.log(valueOfThis);
+
+var favoriteCheese = prompt("What is your favorite cheese?");
+console.log(favoriteCheese + " is your favorite cheese.");
