9b532e6f9e6d9faa8e3570e9f98532dab1e5c7c8
David Culver <culver.david.p@gmail.com>
2021-03-25 13:56:12 -0500

    completed while-loop exercise

--- js/while.js
+++ js/while.js
@@ -1,11 +1,26 @@
 "use strict";
 
-var number = 1;
-var counter = 0
-while (counter < 16){
-    number = number * 2;
-    console.log(number);
-    counter++;
-}
+// var number = 1;
+// var counter = 0
+// while (counter < 16){
+//     number = number * 2;
+//     console.log(number);
+//     counter++;
+// }
 
-//testing git push
+//instructor example
+
+var num = prompt("What number do you want to start with?");
+var originalNumber = num;
+var exponent = 2
+// num = num * originalNumber;
+// console.log(originalNumber + " to the power of " + exponent + " = " + num);
+// exponent = exponent + 1;
+// num = num * originalNumber;
+// console.log(originalNumber + " to the power of " + exponent + " = " + num);
+// exponent = exponent + 1;
+while (exponent < 17){
+    num = num * originalNumber;
+    console.log(originalNumber + " to the power of " + exponent + " = " + num);
+    exponent = exponent + 1;
+}
