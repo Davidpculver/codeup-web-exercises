ffc592278d2f341267fefb80160b1d882302cf0a
David Culver <culver.david.p@gmail.com>
2021-03-26 15:34:37 -0500

    working on for loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// function showMultiplicationTable(num) {
+//     for(var i = 1; i < 11; i++){
+//         console.log(num + " x " + i + " = " + num * i);
+//     }
+// }
+//
+// showMultiplicationTable(7);
+
+
