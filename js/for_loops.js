18b149ed21bac490bfeaf648e9c901da86ab4ee5
David Culver <culver.david.p@gmail.com>
2021-03-26 17:12:33 -0500

    completed for loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Exercises 2;
 // function showMultiplicationTable(num) {
 //     for(var i = 1; i < 11; i++){
 //         console.log(num + " x " + i + " = " + num * i);
@@ -7,5 +8,54 @@
 // }
 //
 // showMultiplicationTable(7);
+
+
+// Exercise 3;
+// for(var i = 0; i < 10; i++) {
+//     var randomNum = Math.floor(Math.random() * 180) + 20;
+//     if(randomNum % 2 === 0){
+//         console.log(randomNum + " is even");
+//     } else {
+//         console.log(randomNum + " is odd");
+//     }
+// }
+//
+// // Same exercise, testing the ternary operator
+// for(var i = 0; i < 10; i++) {
+//     var randomNum = Math.floor(Math.random() * 180) + 20;
+//     var oddEven = (randomNum % 2 === 0) ? randomNum + " is even" : randomNum + " is odd";
+//     console.log(oddEven);
+// }
+
+// Exercise 4;
+
+// for (var i = 1; i < 10; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         console.log(i);
+//     }
+// }
+// Trying to understand nested loops
+// for (var i = 1; i < 10; i++) {
+//     for (var j = 1; j <= i; j++) {
+//         console.log("i = " + i + " j + " + j);
+//     }
+// }
+
+// Exercise 5
+for (var i = 100; i > 0; i = i - 5){
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
