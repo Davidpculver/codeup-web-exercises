967f88ec5cf0685a3203dbd04feb769c5fabae0b
David Culver <culver.david.p@gmail.com>
2021-04-29 14:36:04 -0500

    jquery lectures

---
+++ js/Jquery-lec.js
@@ -0,0 +1,17 @@
+// $("h3").click(function () {
+//     // $(this).next().slideToggle(500);   in paren - how long effect takes
+//     // // $(this).next().fadeToggle(500);
+//     // if( $(this).next().children("img").css("display") ==="none"){
+//     //     $(this).next().children("p").fadeToggle(500);
+//     //     $(this).next().children("img").delay(700).slideToggle(500);
+//     // } else {
+//     //     $(this).next().children("img").slideToggle(500);
+//     //     $(this).next().children("p").delay(700).fadeToggle(500);
+//     // }
+//     $(this).next().animate({
+//         opacity: 0,
+//         width:0,
+//         height: 0
+//     }, 1000);
+//
+// });
