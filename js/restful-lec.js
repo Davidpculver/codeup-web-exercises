3f7bd734f9e96e3fa29f7fc9d50cd2bdd1ed66e8
David Culver <culver.david.p@gmail.com>
2021-05-18 12:30:01 -0500

    put and patch lecture

--- js/restful-lec.js
+++ js/restful-lec.js
@@ -11,16 +11,19 @@
 //     .then(data => console.log(data));
 
 //is the same as below:
-
-// fetch('https://awake-faint-phalange.glitch.me/books')
-//     .then(response => response.json())
-//     .then(books => {
-//         let htmlStr = '';
-//         for(let book of books){
-//             htmlStr = `${htmlStr}<h1>${book.title}</h1><p>by: ${book.author.firstName} ${book.author.lastName}</p>`
-//         }
-//         $('#container').html(htmlStr)
-//     });
+const getBooks = () => {
+    fetch('https://awake-faint-phalange.glitch.me/books')
+    .then(response => response.json())
+    .then(books => {
+        console.log(books)
+        let htmlStr = '';
+        for(let book of books){
+            htmlStr = `${htmlStr}<h1>${book.title}</h1><p>by: ${book.author.firstName} ${book.author.lastName}</p>`
+        }
+        $('#container').html(htmlStr)
+    });
+}
+//
 
 //Get back specific reference to book: added /1 inside the url - attaches to the id of the book.
 //id property most important to have inside an object
@@ -49,9 +52,57 @@ let postOptions = {
     body: JSON.stringify(newBook),
 };
 
-fetch('https://awake-faint-phalange.glitch.me/books', postOptions)
-    .then(response => response.json())
-    .then(book => console.log(book))
+$('#addPercy').click(() => {
+    fetch('https://awake-faint-phalange.glitch.me/books', postOptions)
+        .then(getBooks);
+})
+
+//modify existing record
+// PUT - update entire resource / object at once. "overwrite". if no defn of property that previously existed removed with put and replaced with object being put
+let putThis = {
+    "title": "Percy Jackson & The Sea of Monsters",
+    "author": {
+        "firstName": "Rick",
+        "lastName": "Riordan"
+    }
+}
+
+let putOptions = {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    //sending the info as json string
+    body: JSON.stringify(putThis),
+};
+
+
+fetch("https://awake-faint-phalange.glitch.me/books/6", putOptions).then(getBooks);
+
+// PATCH
+//only updates the property; changes title, keeps author the same. Can change all properties as well, but doesnt have to like PUT does.
+let patchThis = {
+    "title": "Percy Jackson & The Titan"
+}
+
+let patchOptions = {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    //sending the info as json string
+    body: JSON.stringify(patchThis),
+};
+
+//patching to id of 7
+fetch("https://awake-faint-phalange.glitch.me/books/7", patchOptions).then(getBooks);
+
+
+
+getBooks();
+
+
+
 
 
 
