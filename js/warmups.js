351120069272dd89c2b7bf833df342a2f66c05bb
David Culver <culver.david.p@gmail.com>
2021-05-25 09:10:23 -0500

    completed morning exercise

--- js/warmups.js
+++ js/warmups.js
@@ -1,17 +1,21 @@
 'use strict'
 
 
-function fizzBuzz() {
-    for(let i = 1; i <= 30; i++){
-        if(i % 3 === 0 && i % 5 === 0){
-            console.log("FizzBuzz")
-        } else if(i % 3 === 0){
-            console.log("Fizz")
-        } else if(i % 5 === 0){
-            console.log("Buzz")
-        } else {
-            console.log(i)
-        }
-    }
-}
-fizzBuzz();
+// function fizzBuzz() {
+//     for(let i = 1; i <= 30; i++){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log("FizzBuzz")
+//         } else if(i % 3 === 0){
+//             console.log("Fizz")
+//         } else if(i % 5 === 0){
+//             console.log("Buzz")
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+// fizzBuzz();
+
+const returnSeven = () => 7;
+
+console.log(returnSeven());
