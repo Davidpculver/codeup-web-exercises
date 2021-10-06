75582005fe68c5b65bab9c2e377293466059a6dd
David Culver <culver.david.p@gmail.com>
2021-10-06 14:17:24 -0500

    js practice

--- js/warmups.js
+++ js/warmups.js
@@ -63,7 +63,6 @@
 // splitString();
 
 
-
 function reverseSentence(sentence) {
     console.log(sentence.split("").reverse().join(''));
 }
@@ -75,7 +74,7 @@ reverseSentence("This sentence is reversed");
 function sumOfNumber(num) {
     let sum = 0;
     let splitNum = num.toString().split("");
-    for (let i = 0; i < splitNum.length; i++){
+    for (let i = 0; i < splitNum.length; i++) {
         sum += parseInt(splitNum[i]);
     }
     console.log(sum);
@@ -85,36 +84,93 @@ sumOfNumber(123456);
 
 
 function multTable(x) {
-    for (let i = 1; i <= 10; i++){
+    for (let i = 1; i <= 10; i++) {
         console.log(`${x} x ${i} = ${(x * i)}`);
     }
 }
 
 multTable(5);
 
-for (let i = 0; i <= 10; i++){
-    let ranNumb = Math.floor(Math.random() * 10) + 1;
-    if (ranNumb % 2 === 0){
-        console.log(`${ranNumb} is even.`)
-    } else {
-        console.log(`${ranNumb} is odd.`)
+// for (let i = 0; i <= 10; i++){
+//     let ranNumb = Math.floor(Math.random() * 10) + 1;
+//     if (ranNumb % 2 === 0){
+//         console.log(`${ranNumb} is even.`)
+//     } else {
+//         console.log(`${ranNumb} is odd.`)
+//     }
+//     // console.log(ranNumb);
+// }
+
+// function fizzbuzz(num) {
+//     for (let i = 1; i <= num; i++){
+//         if(i % 3 === 0 && i % 5 === 0) {
+//             console.log("Fizzbuzz");
+//         } else if (i % 3 === 0){
+//             console.log("Fizz");
+//         } else if (i % 5 === 0){
+//             console.log("Buzz");
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+//
+// fizzbuzz(50);
+
+function fibonacci(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let num3;
+    console.log(num1);
+    console.log(num2);
+    for (let i = 0; i < num; i++) {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+        console.log(num3);
+
     }
-    // console.log(ranNumb);
+    console.log(`The fibonacci of ${num} is ${num3}! `);
 }
 
-function fizzbuzz(num) {
-    for (let i = 1; i <= num; i++){
-        if(i % 3 === 0 && i % 5 === 0) {
-            console.log("Fizzbuzz");
-        } else if (i % 3 === 0){
-            console.log("Fizz");
-        } else if (i % 5 === 0){
-            console.log("Buzz");
+fibonacci(15);
+
+
+function fibArray(num) {
+    let array = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        array.push(array[i - 1] + array[i - 2]);
+    }
+    console.log(array.join(', '));
+}
+
+fibArray(6);
+
+
+function coins(num) {
+    let counter = 0;
+    console.log(num);
+    for (let i = 0; i < num; i++) {
+        console.log(num);
+        if (num % 5 === 0) {
+            counter += 1;
+            num -= 5;
+            continue;
+        } else if (num % 3 === 0) {
+            counter += 1;
+            num -= 3;
+            continue;
+        } else if (num === 0) {
+            console.log(num);
+            console.log(counter);
         } else {
-            console.log(i)
+            counter += 1;
+            num -= 1;
+            continue;
+            console.log(num);
         }
     }
 }
 
-fizzbuzz(50);
+coins(10);
 
